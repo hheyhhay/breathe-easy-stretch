6fcfc8072cd7e86b4564e77273c7c66bef1ee977
Hayley Dixon <hdixon37@gmail.com>
2021-09-15 15:15:48 -0600

    Convert code to TSX and have application appear on page

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Form.css';
+import { allStates } from '../util/data'
 
 interface IState {
     allStates: string[],
@@ -7,13 +8,37 @@ interface IState {
     selecetedCity: string,
     allCitiesInState: string[],
 }
-class Form extends React.Component<{}, IState> {
-    constructor(props) {
-        super(props);
-        this.state = {
-          
-        }
-    }
+function Form(){
+    const [selectedState, setSelectedState ] =useState('')
+    const [allCitiesInState, setAllCitiesInState ] = useState([])
+    const [selectedCity, setSelectedCity ] = useState('')
+    const [citiesError, setCitiesError ] = useState(false)
+
+const stateOptions = allStates.map((state, index) => {
+    return (<option key={index} value={state}>{state}</option>)
+})
+
+const handleStateChange = (e: React.ChangeEvent<HTMLSelectElement> ) => {
+    setSelectedState(e.target.value)
+}
+
+
+return (
+    <section>
+    <form>
+      <select value={selectedState} onChange={e => handleStateChange(e)}>
+        <option value=''>- Select a State -</option>
+        { stateOptions }
+      </select>
+      <select value={selectedCity} onChange={event => console.log(event)}>
+        <option value=''>Select a City</option>
+        {/* { cityOptions } */}
+      </select>
+      <input type="submit" value="Show AQI" />   
+    </form>
+    <button onClick={event => console.log(event)}>Reset Form</button>
+  </section>
+)
 }
 
 export default Form
