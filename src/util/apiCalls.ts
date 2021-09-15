b126e24ce2dd3e31017a6fbdbb23ed2b599ad481
Eric Li <ericli@knights.ucf.edu>
2021-09-15 11:49:15 -0600

    Add getData function to apiCalls file

---
+++ src/util/apiCalls.ts
@@ -0,0 +1,15 @@
+export const getData = (url: string): Promise<string> => {
+  
+    return fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else if (response.status === 404) {
+          throw Error('404 error - Destination not found.')
+        } else if (response.status === 500) {
+          throw Error('500 error - Something on our end is not working right now.')
+        } else {
+        throw Error('Other error - Something went wrong.')
+        }
+      })
+  }
