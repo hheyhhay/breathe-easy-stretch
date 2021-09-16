673d0ce73ca1f218eea560131411b1407b635a8b
Hayley Dixon <hdixon37@gmail.com>
2021-09-15 18:11:08 -0600

    integerate new apis, typescript and dataclean'

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './Form.css';
 import { allStates } from '../util/data'
+import { getData } from '../apiCalls'
+import { cleanCityData, cleanAllCitiesData } from '../util/dataCleaning'
 
 interface IState {
     allStates: string[],
@@ -8,18 +10,38 @@ interface IState {
     selecetedCity: string,
     allCitiesInState: string[],
 }
+type City = { city: string}
 function Form(){
     const [selectedState, setSelectedState ] =useState('')
-    const [allCitiesInState, setAllCitiesInState ] = useState([])
+    const [allCitiesInState, setAllCitiesInState ] = useState<string[]>([''])
     const [selectedCity, setSelectedCity ] = useState('')
     const [citiesError, setCitiesError ] = useState(false)
+
+    useEffect(() => {
+        if(selectedState) {
+            getCities()
+        }
+    }, [selectedState])
+
+const getCities = () => {
+    getData(`http://api.airvisual.com/v2/cities?state=${selectedState}&country=USA&key=da479dc8-2e38-4a47-97a1-7396f6c348e1`)
+    .then(data => cleanAllCitiesData(data))
+    .then(data => setAllCitiesInState(data))
+}
 
 const stateOptions = allStates.map((state, index) => {
     return (<option key={index} value={state}>{state}</option>)
 })
+const cityOptions = allCitiesInState.map((city, index) => {
+    return (<option key={index} value={city}>{city}</option>)
+})
 
 const handleStateChange = (e: React.ChangeEvent<HTMLSelectElement> ) => {
     setSelectedState(e.target.value)
+}
+
+const handleCityChange = (e: React.ChangeEvent<HTMLSelectElement> ) => {
+    console.log(e.target.value)
 }
 
 
@@ -30,9 +52,9 @@ return (
         <option value=''>- Select a State -</option>
         { stateOptions }
       </select>
-      <select value={selectedCity} onChange={event => console.log(event)}>
+      <select value={selectedCity} onChange={e => handleCityChange(e)}>
         <option value=''>Select a City</option>
-        {/* { cityOptions } */}
+        { cityOptions }
       </select>
       <input type="submit" value="Show AQI" />   
     </form>
