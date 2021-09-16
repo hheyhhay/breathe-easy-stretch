5ba7a29a1cb2d6fb026e42838e0cff929c45a5bc
Hayley Dixon <hdixon37@gmail.com>
2021-09-15 18:09:48 -0600

    Add simplified API calls and integreate typescript into promise

--- src/apiCalls.ts
+++ src/apiCalls.ts
@@ -1,22 +1,16 @@
-//Not currently being used
-// export   const getCurrentOtherCityData = (currentOtherCity, selectedState) => {
-//    return fetch(`http://api.airvisual.com/v2/city?city=${currentOtherCity}&state=${selectedState}&country=USA&key=da479dc8-2e38-4a47-97a1-7396f6c348e1`)
-//       .then(response => response.json())
-//       .then(data => setOtherCitiesData(...otherCitiesData, data.data))
-//       .catch(error => setOtherCitiesDataError(error.message))
-//   }
-
-//   export const getCurrentLocationData = () => {
-//    return fetch(`http://api.airvisual.com/v2/nearest_city?key=da479dc8-2e38-4a47-97a1-7396f6c348e1`) // THIS IS HARDCODED PIECE OF DATA ->  how to get IP address from click?
-//     .then(response => response.json())
-//     .then(data => setSelectedCity(data.data))
-//     .catch(error => setCityDataError(error.message))
-//   }
-
-//   export const getSelectedCityData = (selectedCity, selectedState) => {
-//     fetch(`http://api.airvisual.com/v2/city?city=${selectedCity}&state=${selectedState}&country=USA&key=da479dc8-2e38-4a47-97a1-7396f6c348e1`)
-//     .then(response => response.json())
-//     .then(data => setSelectedCity(data.data))
-//     .catch(error => setCityDataError(error.message))
-//   }
-export {}
+import { CityObj, CityList } from "./util/dataCleaning"
+  export const getData = (url: string): Promise<CityList> => {
+  
+    return fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else if (response.status === 404) {
+          throw Error('404 error - Destination not found.')
+        } else if (response.status === 500) {
+          throw Error('500 error - Something on our end is not working right now.')
+        } else {
+        throw Error('Other error - Something went wrong.')
+        }
+      })
+  }
