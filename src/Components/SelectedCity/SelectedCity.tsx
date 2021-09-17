939197a68a2c346e079208ab395bd9c943859e0f
Eric Li <ericli@knights.ucf.edu>
2021-09-17 12:01:55 -0600

    Add semantics and JSX conditionals

--- src/Components/SelectedCity/SelectedCity.tsx
+++ src/Components/SelectedCity/SelectedCity.tsx
@@ -10,17 +10,24 @@ interface Props {
 
 const SelectedCity: React.FC<Props> = ( {selectedCityData} ) => {
     return (
-        <section className='selected-city-card'>
+            <section className='selected-city-card'>
             <p className='location-title'>I'm in</p>
             <h2 className='location-info'>{`${selectedCityData.city}, ${selectedCityData.state}`}</h2>
             <p className='aqi-title'>Current Air Quality</p>            
             <p className='aqi-timestamp'>Last Updated {dayjs(selectedCityData.timeStamp).format('MM/DD/YYYY')}</p>
-            { selectedCityData.aqi && <h3 className='aqi-value'>{`${selectedCityData.aqi} AQI`}</h3> }
+            { selectedCityData.aqi < 51 && <div><h3 className='aqi-value green'>{`${selectedCityData.aqi} AQI`}</h3><p className='aqi-rating green'>Good</p></div> }
+            { (selectedCityData.aqi >= 51 && selectedCityData.aqi < 101) && <div><h3 className='aqi-value yellow'>{`${selectedCityData.aqi} AQI`}</h3>
+                <p className='aqi-rating yellow'>Moderate</p></div> }
+            { (selectedCityData.aqi >= 101 && selectedCityData.aqi < 151) && <div><h3 className='aqi-value orange'>{`${selectedCityData.aqi} AQI`}</h3>
+                <p className='aqi-rating orange'>Sensitive</p></div>}
+            { selectedCityData.aqi >= 151 && <div><h3 className='aqi-value red'>{`${selectedCityData.aqi} AQI`}</h3>
+                <p className='aqi-rating red'>Unhealthy</p></div> }
             <p className='select-prompt'>Pick a city to compare</p>
             <Link to={`/`}>
                 <button className='home-button'>Return Home</button>
             </Link>
-        </section>
+            </section>
+        
     )
 }
 
