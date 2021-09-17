ce26742bae7719d5bb0fe748943a98a5d93d6dc9
Eric Li <ericli@knights.ucf.edu>
2021-09-17 09:35:31 -0600

    Add semantics to JSX

--- src/Components/SelectedCity/SelectedCity.tsx
+++ src/Components/SelectedCity/SelectedCity.tsx
@@ -1,29 +1,27 @@
-import React from 'react'
-import './SelectedCity.css'
+import React from 'react';
+import './SelectedCity.css';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import { CleanData } from '../../util/dataCleaning';
 
-
 interface Props {
     selectedCityData: CleanData
-
 }
+
 const SelectedCity: React.FC<Props> = ( {selectedCityData} ) => {
     return (
-        <section>
-         <p> I'm in </p>
-        <h2>{`${selectedCityData.city}, ${selectedCityData.state}`} </h2>
-        <p>Current Air Quality</p>            <p>Last Updated {dayjs(selectedCityData.timeStamp).format('MM/DD/YYYY')} </p>
-         <h3>{`${selectedCityData.aqi} AQI`}</h3>
-        <p>Pick city to compare:</p>
-        <Link
-        to={`/`}
-        >
-        <button>Return Home</button>
-      </Link>
-    </section>
+        <section className='selected-city-card'>
+            <p className='location-title'>I'm in</p>
+            <h2 className='location-info'>{`${selectedCityData.city}, ${selectedCityData.state}`}</h2>
+            <p className='aqi-title'>Current Air Quality</p>            
+            <p className='aqi-timestamp'>Last Updated {dayjs(selectedCityData.timeStamp).format('MM/DD/YYYY')}</p>
+            { selectedCityData.aqi && <h3 className='aqi-value'>{`${selectedCityData.aqi} AQI`}</h3> }
+            <p className='select-prompt'>Pick a city to compare</p>
+            <Link to={`/`}>
+                <button className='home-button'>Return Home</button>
+            </Link>
+        </section>
     )
 }
 
-export default SelectedCity
+export default SelectedCity;
