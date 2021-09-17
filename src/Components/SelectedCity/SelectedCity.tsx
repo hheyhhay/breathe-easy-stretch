db6918deec0ef1bb9d108978a6b0b4ba6943337a
Hayley Dixon <hdixon37@gmail.com>
2021-09-16 19:59:35 -0600

    Integrate router, write JSX, and destructure props

--- src/Components/SelectedCity/SelectedCity.tsx
+++ src/Components/SelectedCity/SelectedCity.tsx
@@ -2,3 +2,28 @@ import React from 'react'
 import './SelectedCity.css'
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
+import { CleanData } from '../../util/dataCleaning';
+
+
+interface Props {
+    selectedCityData: CleanData
+
+}
+const SelectedCity: React.FC<Props> = ( {selectedCityData} ) => {
+    return (
+        <section>
+         <p> I'm in </p>
+        <h2>{`${selectedCityData.city}, ${selectedCityData.state}`} </h2>
+        <p>Current Air Quality</p>            <p>Last Updated {dayjs(selectedCityData.timeStamp).format('MM/DD/YYYY')} </p>
+         <h3>{`${selectedCityData.aqi} AQI`}</h3>
+        <p>Pick city to compare:</p>
+        <Link
+        to={`/`}
+        >
+        <button>Return Home</button>
+      </Link>
+    </section>
+    )
+}
+
+export default SelectedCity
