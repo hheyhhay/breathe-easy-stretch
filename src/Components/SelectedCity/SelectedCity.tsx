4e9cdadf2645ee83e843a29b3af3a201e8799816
Eric Li <ericli@knights.ucf.edu>
2021-09-18 10:43:56 -0600

    Update styling for selected city nav and form

--- src/Components/SelectedCity/SelectedCity.tsx
+++ src/Components/SelectedCity/SelectedCity.tsx
@@ -21,8 +21,7 @@ const SelectedCity: React.FC<Props> = ({ selectedCityData, resetCityData }) => {
       { (aqi >= 51 && aqi < 101) && <div><h3 className='aqi-value yellow'>{`${aqi} AQI`}</h3><p className='aqi-rating yellow'>Moderate</p></div> }
       { (aqi >= 101 && aqi < 151) && <div><h3 className='aqi-value orange'>{`${aqi} AQI`}</h3><p className='aqi-rating orange'>Sensitive</p></div>}
       { aqi >= 151 && <div><h3 className='aqi-value red'>{`${aqi} AQI`}</h3><p className='aqi-rating red'>Unhealthy</p></div> }
-      <p className='temp-title'>Current Temperature</p> 
-      <p className='temp-value'>{`${temperature} F`}</p>
+      <p className='temp-value'>{`${temperature}°F`}</p>
       <p className='select-prompt'>Pick a city to compare</p>
       <Link to={`/`}>
         <button className='home-button' onClick={() => resetCityData()}>Return Home</button>
