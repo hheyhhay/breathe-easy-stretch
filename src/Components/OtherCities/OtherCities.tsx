c1522c1949b33e21fb5f89a037f11421e61eb567
William Phelps <williamphelps13@gmail.com>
2021-09-17 19:54:09 -0600

    Create component to hold and map other cities

---
+++ src/Components/OtherCities/OtherCities.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './OtherCities.css'
+import OtherCity from '../OtherCity/OtherCity'
+import { CleanData } from '../../util/dataCleaning'
+
+interface Props {
+  otherCitiesData: CleanData[]
+  deleteCityData: any
+  selectedCityData: any
+}
+
+const OtherCities: React.FC<Props> = ({ otherCitiesData, deleteCityData, selectedCityData}) => {
+  
+  const otherCityCards = otherCitiesData.map(otherCity => {
+    return (
+      <OtherCity
+        city={otherCity.city}
+        state={otherCity.state}
+        aqi={otherCity.aqi}
+        timeStamp={otherCity.timeStamp}
+        temperature={otherCity.temperature}
+        location={otherCity.location}
+        key={otherCity.location}
+        deleteCityData={deleteCityData}
+        selectedCityData={selectedCityData}
+      />
+    )
+  })
+
+  return (
+    <section>
+      {otherCityCards}
+    </section>
+  )
+}
+
+export default OtherCities
