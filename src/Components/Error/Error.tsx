b325e5030ac3977cb163e53a1f1064b43b6179fa
Hayley Dixon <hdixon37@gmail.com>
2021-09-20 15:00:28 -0600

    Add styling to error and loading components

--- src/Components/Error/Error.tsx
+++ src/Components/Error/Error.tsx
@@ -11,16 +11,18 @@ interface PropsError {
 const Error: React.FC<PropsError>= ({ dataContents, message }) => {
   return (
     <div className='error-container'>
-      <h2 className='error-heading'>We've encountered an error in retrieving the {dataContents}.</h2>
-      <p className='error-message'>{message}</p>
-      <img
-        alt='Error image'
-        className='error-image'
-        src={errorImage}
-      />
-      <Link to={`/`}>
-        <button className='error-button'>Return Home</button>
-      </Link>
+      <div className='error-card'>
+        <h2 className='error-heading'>We've encountered an error in retrieving the {dataContents}.</h2>
+        <p className='error-message'>{message}</p>
+        <img
+          alt='Error image'
+          className='error-image'
+          src={errorImage}
+        />
+        <Link to={`/`}>
+          <button className='error-button'>Return Home</button>
+        </Link>
+      </div>
     </div>
   )
 }
