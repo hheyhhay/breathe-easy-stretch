356d87c24d78ddef88fbc68758ba2f0b574a00c2
William Phelps <williamphelps13@gmail.com>
2021-09-20 14:47:46 -0600

    Add currentCityFetch

--- src/Components/SelectedCity/SelectedCity.tsx
+++ src/Components/SelectedCity/SelectedCity.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './SelectedCity.css';
 import { Link } from 'react-router-dom';
 import { CleanData } from '../../util/dataCleaning';
+import { getCityData } from '../../apiCalls';
+import {  cleanCityData } from '../../util/dataCleaning'
 
 interface PropsSelectedCity {
     selectedCityData: CleanData
     resetCityData: any
-}
+    setCityDataError: any
+    current: boolean
+    setSelectedCityData: any
+  }
 
-const SelectedCity: React.FC<PropsSelectedCity> = ({ selectedCityData, resetCityData }) => {
-  const {city, state, timeStamp, aqi, temperature} = selectedCityData
+const SelectedCity: React.FC<PropsSelectedCity> = ({ selectedCityData, resetCityData, setCityDataError, current, setSelectedCityData }) => {
+  const [currentCityData, setCurrentCityData] = useState<CleanData | any>(0)  
+  let city, state, timeStamp, aqi, temperature;
+  
+  ({city, state, timeStamp, aqi, temperature} = selectedCityData)
+
+  if(current) {
+    ({city, state, timeStamp, aqi, temperature} = currentCityData)
+  }
+
+  useEffect(() => {
+    if (current) {
+      getCityData(`http://api.airvisual.com/v2/nearest_city?key=8b1bc68f-68fc-497f-8392-79664f6b493f`)
+      .then(data => cleanCityData(data))
+      .then(data => {
+        setCurrentCityData(data)
+        setSelectedCityData(data)
+        })
+      .catch(error => setCityDataError(error.message))
+    }
+  }, [current])
 
   return (
     <section className='selected-city-card'>
