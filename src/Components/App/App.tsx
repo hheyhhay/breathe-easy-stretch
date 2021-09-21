f241c96227dd4963ad7e102bb774547bae15e0a9
Eric Li <ericli@knights.ucf.edu>
2021-09-20 20:32:11 -0600

    Edit duplicate error

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -37,7 +37,7 @@ const App: React.FunctionComponent = () => {
     if (!checkIfIncludesCity.length) {
       setOtherCitiesData([...otherCitiesData, data].sort((a, b) => a.aqi - b.aqi))
     } else {
-      setDuplicateCityError('This city is already being compared.')
+      setDuplicateCityError('This city is already being compared, dumbass.')
       setTimeout(() => {
         setDuplicateCityError('')
       }, 3000);
